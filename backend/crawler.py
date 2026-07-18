5f07a68403a4a31aaeb893ca01f2e5ff255de32b
optivisionlab <optivision.work@gmail.com>
2026-07-18 17:00:45 +0700

    update token

--- backend/crawler.py
+++ backend/crawler.py
@@ -2,6 +2,8 @@ import httpx
 import json
 import asyncio
 
+TOKEN_AUTHEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjowLCJhdWQiOiJndWVzdCIsImV4cCI6MTc4NDQ1NTA5OSwiaWF0IjoxNzg0MzY4Njk5LCJpc3MiOiJiZS1kZWxpdmVyeS1nYXRld2F5In0.cQ_Q9rKG8NvLp1JsSL9Ote9nl6JO4orVNxJ8X1dS_aM"
+
 async def crawl_befood_restaurants(page=1, limit=12):
     """
     Fetches restaurant data from the befood API with pagination asynchronously.
@@ -12,7 +14,7 @@ async def crawl_befood_restaurants(page=1, limit=12):
         'accept': '*/*',
         'accept-language': 'vi,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
         'app_version': '11310',
-        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjowLCJhdWQiOiJndWVzdCIsImV4cCI6MTc2MzAxMDMxMiwiaWF0IjoxNzYyOTIzOTEyLCJpc3MiOiJiZS1kZWxpdmVyeS1nYXRld2F5In0.OmRVNHFltVVgiVttiZ85NP5KV-NYwuIX3xn9n7SQkto',
+        'authorization': f'Bearer {TOKEN_AUTHEN}',
         'cache-control': 'no-cache',
         'content-type': 'application/json',
         'origin': 'https://food.be.com.vn',
@@ -78,7 +80,7 @@ async def crawl_restaurant_detail(restaurant_id: int):
         'accept': '*/*',
         'accept-language': 'vi,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
         'app_version': '11310',
-        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjowLCJhdWQiOiJndWVzdCIsImV4cCI6MTc2MzAxMDMxMiwiaWF0IjoxNzYyOTIzOTEyLCJpc3MiOiJiZS1kZWxpdmVyeS1nYXRld2F5In0.OmRVNHFltVVgiVttiZ85NP5KV-NYwuIX3xn9n7SQkto',
+        'authorization': f'Bearer {TOKEN_AUTHEN}',
         'cache-control': 'no-cache',
         'content-type': 'application/json',
         'origin': 'https://food.be.com.vn',
